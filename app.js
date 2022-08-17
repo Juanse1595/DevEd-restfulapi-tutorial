b6e46097212e73586b1ee06714cd451f426204ae
Juanse1595 <jspbescos15@gmail.com>
2022-08-16 23:22:28 -0500

    ADD app listens to port 3000 and has a get route at home

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// Start listening to requests
+
+app.listen(3000);
+
 // ROUTES
 
+app.get("/", (req, res) => {
+  res.send("This is home boyyyy");
+});
