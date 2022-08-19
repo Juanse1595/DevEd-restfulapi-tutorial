20a72abd2ef7d186a5b93efd0d818dc4e16b25df
Juanse1595 <jspbescos15@gmail.com>
2022-08-18 20:16:02 -0500

    ADD .env file to manage env variables, routes dir to have the routes in specific files, posts.js to handle post endpoints, UPD app.js used env variables for DB uri

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
 
@@ -27,6 +28,6 @@ app.get("/posts", (req, res) => {
 
 
 // Connect to DB
-mongoose.connect("mongodb+srv://juanse1595:jspbfc1995@test.fir3x0y.mongodb.net/?retryWrites=true&w=majority", () => {
+mongoose.connect(process.env.DB_URL, () => {
   console.log("connected to db");
 })
